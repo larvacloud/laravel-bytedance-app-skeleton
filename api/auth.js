217495f47e9f45eb49bb573dcaeb41b0d286f1c1
XuTongle <xutongle@gmail.com>
2020-07-01 19:23:18 +0800

    add api

---
+++ api/auth.js
@@ -0,0 +1,40 @@
+import request from './request'
+import config from '../config'
+
+/**
+ * 小程序登录，将 tt.login 获取的 code 和 tt.getUserInfo 获取的 user_info 一并提交给服务器
+ * 如果社交账户的 code 验证通过，则user_info是可信的
+ */
+export function login(code, userInfo) {
+    return request({
+        method: 'POST',
+        url: '/oauth/token',
+        data: {
+            grant_type: "mini-program",
+            client_id: config.appId,
+            client_secret: config.appSecret,
+            provider: "bytedance_mini_program",
+            code: code,
+            user_info: {
+                nickname : userInfo.nickName,
+                avatar : userInfo.avatarUrl,
+                gender:userInfo.gender
+            }
+        }
+    })
+}
+
+export function logout() {
+    if (getApp().globalData.userInfo) {
+        getApp().globalData.userInfo = '';
+        tt.reLaunch({
+            url: '/pages/index/index'
+        })
+    } else {
+        tt.showToast({
+            title: '注销失败!',
+            icon: 'warn',
+            duration: 1000,
+        });
+    }
+}
